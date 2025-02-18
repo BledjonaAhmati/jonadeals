bd31fe11c48c7b4c07a5c5e1d457757ba79d48cb
Jonadeals <ahmati.bau@gmail.com>
2025-02-18 08:51:47 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-cd jonadeals
-git add .
-git commit -m "Upload React Native project"
-git push -u origin main
-nano src/App.js
-git branch -M main
-git remote add origin https://github.com/jonadeals/jonadeals.git
-git push -u origin main
-git config --global user.name "Jonadeals" 
-git config --global user.email "ahmati.bau@gmail.com" 
+function App() {
+    return (
+        <div>
+            <h1>Hello from React App!</h1>
+        </div>
+    );
+}
+
+export default App;
+
